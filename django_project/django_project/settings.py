3a6ac21320e3bc8a6f621998f5993f232ddff360
Athena Liu <sdathena@mail.ubc.ca>
2019-04-08 16:34:25 -0700

    Update settings and Procfile

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w7v(!q%d39tq#*mb^z-7psr3aa7(n(a1_7vd5ruftc#akd4x+0'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-bloggers.herokuapp.com']
 
 
 # Application definition
